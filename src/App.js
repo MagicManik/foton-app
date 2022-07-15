28ed5227b099ed495155b27f94d3d480a82930d8
MagicManik <manikislam1952@gmail.com>
2022-07-15 19:54:22 +0600

    react router tailwind with daisyui setup done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <button class="btn btn-primary">Button</button>
+    </div>
+  );
+}
+
+export default App;
